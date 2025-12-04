85e8c4751327aa06a8e71fe22f8d50c885988528
Yousef Osama <yo5739240@gmail.com>
2025-12-04 13:44:39 +0200

    Initial project commit

--- hospital_backend/settings.py
+++ hospital_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d*(1s56#h&3o%olz%l+14!^po&#m^^mm_mnfxch)5*56sag5)e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
