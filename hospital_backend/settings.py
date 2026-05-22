e2f4681ab80b929d796c4c91c37c85221966f920
Yousef Osama <yo5739240@gmail.com>
2026-05-22 21:13:26 +0300

    first commit

--- hospital_backend/settings.py
+++ hospital_backend/settings.py
@@ -125,5 +125,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'Ahmedknawy110@gmail.com'    
-EMAIL_HOST_PASSWORD = 'tcqj topp wwcp egql'   
+EMAIL_HOST_USER = 'Ahmedknawy110@gmail.com'
+EMAIL_HOST_PASSWORD = 'fpoz jrte spgp aouk'   
