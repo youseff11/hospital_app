42ccbb8959c02e89f48387a3bff211b0602e9453
Yousef Osama <yo5739240@gmail.com>
2026-06-12 15:44:17 +0300

    first commit

--- hospital_backend/wsgi.py
+++ hospital_backend/wsgi.py
@@ -8,8 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+# استيراد مكتبة تفعيل ملفات الـ .env
+try:
+    from dotenv import load_dotenv
+    # تحديد مسار ملف الـ .env (في نفس مستوى ملف manage.py)
+    project_folder = os.path.expanduser('/home/youseff11/hospital_backend') # تأكد من اسم اليوزر والمجلد على السيرفر
+    load_dotenv(os.path.join(project_folder, '.env'))
+except ImportError:
+    pass
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital_backend.settings')
 
