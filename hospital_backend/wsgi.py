b3a6e962c2639d6ac3981719c708333ae025ef86
Yousef Osama <yo5739240@gmail.com>
2026-06-12 16:07:59 +0300

    first commit

--- hospital_backend/wsgi.py
+++ hospital_backend/wsgi.py
@@ -8,16 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-from django.core.wsgi import get_wsgi_application
 
-# محاولة تحميل متغيرات البيئة من ملف .env بشكل آمن
-try:
-    from dotenv import load_dotenv
-    # تأكد أن هذا هو المسار الصحيح لمجلد الـ backend اللي جواه ملف .env
-    project_folder = '/home/youseff11/hospital_backend' 
-    load_dotenv(os.path.join(project_folder, '.env'))
-except Exception as e:
-    print(f"Dotenv loading failed: {e}")
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital_backend.settings')
 
