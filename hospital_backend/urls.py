812f72a753ab9b4e1aac73dd00d1d5c1a225e961
Yousef Osama <yo5739240@gmail.com>
2026-06-16 04:47:17 +0300

    first commit

--- hospital_backend/urls.py
+++ hospital_backend/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings          # ← ده المهم
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
